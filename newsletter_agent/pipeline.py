58648292243cb6aa000c7f31ecc0545be6a79b16
Mert Can Dogusoy <mertcandogusoy@Merts-MacBook-Air.local>
2026-05-15 13:47:27 +0200

    auto: 2026-05-15 13:47

--- newsletter_agent/pipeline.py
+++ newsletter_agent/pipeline.py
@@ -18,6 +18,7 @@ from newsletter_agent.specialists.eurostat import fetch_eurostat
 from newsletter_agent.specialists.worldbank import fetch_worldbank
 from newsletter_agent.processors.normalize import drop_nulls, align_dates, index_to_100, compute_yoy
 from newsletter_agent.processors.converters import apply_conversions
+from newsletter_agent.formatting import fmt_da, EXCEL_NUM_FORMAT
 from newsletter_agent.routing import get_routing_hint
 from newsletter_agent.renderers.charts import render_type_a, render_type_b, render_type_c, render_type_e, render_type_f, render_type_g, render_type_p
 from newsletter_agent.renderers.tables import render_type_d
