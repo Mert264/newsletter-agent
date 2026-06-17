3b79df123b282b4ee040d519ef85d1b673f2aec2
Mert Can Dogusoy <mcd@majinvest.com>
2026-06-17 17:11:05 +0200

    auto: 2026-06-17 17:11

--- newsletter_agent/specialists/annual_report_kpi.py
+++ newsletter_agent/specialists/annual_report_kpi.py
@@ -154,9 +154,12 @@ def build_chart_specs(
     base_price = base_sc["price"]
     bear_price = bear_sc["price"]
     bull_price = bull_sc["price"]
-    _all_none  = all(v is None for v in (base_price, bear_price, bull_price))
-    if _all_none:
-        base_price = bear_price = bull_price = 0.0
+    if base_price is None:
+        base_price = 0.0
+    if bear_price is None:
+        bear_price = 0.0
+    if bull_price is None:
+        bull_price = 0.0
 
     t = STATUTORY_TAX_RATE.get(iso3, STATUTORY_TAX_RATE["_default"])
 
